ce79909773ef5f43d8faf461ba2e04cb222710b2
Leo Sei <sei7787@gmail.com>
2012-10-20 14:44:04 +0200

    dataModel GameState creation

---
+++ backend/Game.py
@@ -0,0 +1,31 @@
+"""
+Game object
+""""
+
+import datetime
+import logging
+import os
+import random
+import re
+from google.appengine.ext import db
+
+# Data Model
+class GameState(db.Model):
+  """All the data we store for a game"""
+  partyHash = db.StringProperty()
+  currentPlayer = db.StringProperty()
+  totalPlayer = dbIntegerProperty()
+  scorePlayers =  db.StringListProperty()
+
+# Game controler object
+class Game():
+
+    def __init__(self):
+
+    def push(self,GameState):
+        GameState.put()        
+        
+    def pull(self,partyHash):
+        self.game = Game.get_by_key_name(partyHash)
+        return game
+        
