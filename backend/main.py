366ea753437ef845d7b07d635e4237fb2c81b082
gusguslukifer <augustin.riedinger@gmail.com>
2012-10-20 03:47:26 +0200

    message from mobile is in logged on tv

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from google.appengine.ext.webapp import template
+from google.appengine.api import channel
 import webapp2
 import os
 
@@ -20,19 +21,49 @@ class MainRouter(webapp2.RequestHandler):
 
 class TvRouter(webapp2.RequestHandler):
     def get(self):
+        
+        token = channel.create_channel('hash'+'tv')
+
         template2handler(self,'index-tv.html',{
-                                           'title': 'You are the TV !'
-                                           })      
+                                           'title': 'You are the TV !',
+                                           'token': token
+                                           })
+    def post(self):
+#       self.response.headers['Content-Type'] = 'text/json'
+#        self.response.out.write('{"message":"How are you ?"}')
+##        self.response.out('Salut')
+
+        channel.send_message('hash'+'tv','{"message":"How are you ?"}')
+#        channel.send_message('hash'+'tv','How are you?')
+
+        if (self.request.get('message')):
+            channel.send_message('hash'+'tv',self.request.get('message'))
   
 class mobileRouter(webapp2.RequestHandler):
     def get(self):
+        
+        token = channel.create_channel('hash'+'mobile')
+
         template2handler(self,'index-mobile.html',{
-                                           'title': 'You are the player!'
-                                           })  
+                                           'title': 'You are the player!',
+                                           'token': token
+                                           })
+        if (self.request.get('message')):
+            channel.send_message('hash'+'tv',self.request.get('message'))
+        
+    def post(self):
+        if (self.request.get('message')):
+            channel.send_message('hash'+'tv',self.request.get('message'))
+        if (self.request.get('coordinates')):
+            channel.send_message('hash'+'tv',self.request.get("coordinates")) 
+            
+        
+
+
                 
 app = webapp2.WSGIApplication([
                                ('/', MainRouter),
-                               ('/.*/tv', TvRouter),             
+                               ('/.*/tv', TvRouter),
                                ('/.*/mobile/player1', mobileRouter),
                                ('/.*/mobile/player2', mobileRouter)
                                ], debug=True)
--- js/main-tv.js
+++ js/main-tv.js
@@ -1,4 +1,55 @@
 window.onload = function() {
+	channel(token);
+	
+	
+	drawSquare();
+	$('#canvas').click(clickCanvas);
+};
+
+function channel(token) {
+	channel = new goog.appengine.Channel(token);
+	socket = channel.open();
+	socket.onopen = onOpened;
+	socket.onmessage = onMessage;
+//	socket.onerror = onError;
+//	socket.onclose = onClose;
+};
+
+sendMessage = function(path, opt_param) {
+//	path += '?g=test';
+	if (opt_param) {
+		path += '&' + opt_param;
+	}
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', path, true);
+	xhr.send();
+};
+
+onOpened = function() {
+	connected = true;
+	sendMessage('tv');
+	// updateBoard();
+	console.log('connection opened');
+};
+
+onMessage = function(message) {
+	console.log(message);
+	$('#message-receiver').html('The serveur sent us :'+message.data);
+	
+	drawCoordinates(message);
+	
+};
+
+clickCanvas = function() {
+	console.log('clicked');
+	sendMessage('tv');
+};
+
+drawCoordinates = function(coordinates) {
+	console.log('Je dessine les coordonnes :'+coordinates);
+};
+
+function drawSquare() {
 	var canvas = document.getElementById("canvas");
 	var context = canvas.getContext("2d");
 
